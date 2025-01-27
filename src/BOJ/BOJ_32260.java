c83b180a48b301e5c7b4d0a2f1c413b74c78a8b5
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-01-27 23:51:24 +0900

    Feat: BOJ_32260 add

---
+++ src/BOJ/BOJ_32260.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+public class BOJ_32260 {
+    public static int sum(int A, int B) {
+        return A + B;
+    }
+
+    public static void main(String[] args) {
+        // 예제 호출
+        int A = 5;
+        int B = 10;
+        System.out.println("Sum: " + sum(A, B));
+    }
+}
