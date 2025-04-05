cb72cecc437e6a2dcee9a88672293a05cbc5b25b
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-05 16:20:29 +0900

    Feat: Programmus_전화번호목록 add

---
+++ src/Programmus/전화번호목록.java
@@ -0,0 +1,33 @@
+package Programmus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 전화번호목록 {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(solution(phone_book));
+    }
+
+
+    public static boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+
+        // 모든 phone 번호 hashMap에 담기
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            // 전화번호의 길이만큼 순회
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                // 해당 전화번호의 접두어가 key로 가진 경우
+                if (map.containsKey(phone_book[i].substring(0,j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
